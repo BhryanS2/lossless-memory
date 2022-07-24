4eb0ea50d5e1ed140e915db229c6625b4ae89a97
BhryanS2 <bhryanstepenhen@gmail.com>
2022-07-24 20:56:50 -0300

    sera

--- frontend/src/services/API/ConfigApi.ts
+++ frontend/src/services/API/ConfigApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BaseURL = "https://lossless-memory.herokuapp.com/";
+const BaseURL = "https://lossless-memory.herokuapp.com";
 
 export const BaseApi = axios.create({
   baseURL: BaseURL,
